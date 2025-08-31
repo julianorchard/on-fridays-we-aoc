ae222081b3fa48b24ef65911f58709447bce7921
Julian Orchard <julian.orchard@codewizards.co.uk>
2025-08-31 21:55:58 +0100

    refactor: That's nicer
    Signed-off-by: Julian Orchard <julian.orchard@codewizards.co.uk>

--- 2015/rust/day3/src/main.rs
+++ 2015/rust/day3/src/main.rs
@@ -3,39 +3,23 @@ use std::fs;
 const INPUT_PATH: &str = "./inputs";
 const DAY: &str = "input";
 
+fn s(c: char, mut x: i32, mut y: i32) -> (i32, i32) {
+    match c {
+        '^' => y = y + 1,
+        'v' => y = y - 1,
+        '>' => x = x + 1,
+        '<' => x = x - 1,
+        _ => {
+            println!("Unexpected character: '{}'", c);
+        }
+    }
+
+    return (x, y);
+}
+
 fn main() {
     let file: String = fs::read_to_string(format!("{}/{}.txt", INPUT_PATH, DAY))
         .expect("Failed to read input file");
-
-    let north = '^';
-    let south = 'v';
-    let east = '>';
-    let west = '<';
-
-    // let (mut x, mut y) = (0, 0);
-    // let mut uniq: i32 = 0;
-    // let mut visited: Vec<String> = Vec::new();
-    //
-    // for c in file.chars() {
-    //     if c == north {
-    //         y = y + 1;
-    //     } else if c == south {
-    //         y = y - 1;
-    //     } else if c == east {
-    //         x = x + 1;
-    //     } else if c == west {
-    //         x = x - 1;
-    //     } else {
-    //         println!("Unexpected character: '{}'", c);
-    //     }
-    //
-    //     if !visited.contains(&format!("{}, {}", x, y)) {
-    //         visited.push(format!("{}, {}", x, y));
-    //         uniq = uniq + 1;
-    //     }
-    // }
-    //
-    // println!("Unique houses visited: {}", uniq)
 
     let (mut sum, mut sx, mut sy, mut rx, mut ry, mut uniq) = (0, 0, 0, 0, 0, 0);
     let mut visited: Vec<String> = Vec::new();
@@ -48,36 +32,12 @@ fn main() {
 
         if sum % 2 == 1 {
             // santa
-            if c == north {
-                sy = sy + 1;
-            } else if c == south {
-                sy = sy - 1;
-            } else if c == east {
-                sx = sx + 1;
-            } else if c == west {
-                sx = sx - 1;
-            } else {
-                println!("Unexpected character: '{}'", c);
-            }
-
-            x = sx;
-            y = sy;
+            (x, y) = s(c, sx, sy);
+            (sx, sy) = (x, y);
         } else {
             // robot santa
-            if c == north {
-                ry = ry + 1;
-            } else if c == south {
-                ry = ry - 1;
-            } else if c == east {
-                rx = rx + 1;
-            } else if c == west {
-                rx = rx - 1;
-            } else {
-                println!("Unexpected character: '{}'", c);
-            }
-
-            x = rx;
-            y = ry;
+            (x, y) = s(c, rx, ry);
+            (rx, ry) = (x, y);
         }
 
         if !visited.contains(&format!("{}, {}", x, y)) {
